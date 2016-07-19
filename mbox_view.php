b4eab8d2585a3f247f4f8ff2f52e4e2f96e733f7
dglinder <dan@linder.org>
2016-07-18 21:20:21 -0500

    Update mbox_view.php

--- mbox_view.php
+++ mbox_view.php
@@ -18,6 +18,10 @@ echo '<head><title>Form:'.$find_str.'</title></head><body>
 <h1>Enter word to search for</h1>
 	<form method="post" action="test_mail.php">
   	<input type="text" name="find_str">'.$find_str.'</input>
+  	<input type="text" name="start_year">'.$start_year.'</input>
+  	<input type="text" name="start_month">'.$start_month.'</input>
+  	<input type="text" name="start_index">'.$start_index.'</input>
+
   	<input type="submit">
  </form>
 </body>';
@@ -31,48 +35,20 @@ if ( ! empty($find_str) ) {
 	$start_year  = $_POST["start_year"] || -1;
 	
 	# Sanitize the year
-	if (is_int( (int)$start_year )) {
-		// We know this is an integer so massage it into what we need.
-		$start_year = (int)$start_year;
-		if ($start_year < $archive_start_year) {
-			$start_year = $archive_start_year;
-		}
-	} else {
-		// Set a sane value
-		$start_year = $archive_start_year;
-	}
+	$start_year = sanitize_int($start_year, $archive_start_year, $archive_start_year, PHP_INT_MAX);
 	$start_year = sprintf("%04d", $start_year);
 
 	# Sanitize the month
-	if (is_int( (int)$start_month )) {
-		// We know this is an integer so massage it into what we need.
-		$start_month = (int)$start_month;
-		if ( ($start_month < 1) or ($start_month > 12) ) {
-			$start_month = 1;
-		}
-	} else {
-		// Set a sane value
-		$start_month = 1;
-	}
+	$start_month = sanitize_int($start_month, 1, 1, 12);
 	$start_month = sprintf("%04d", $start_month);
 
 	# Sanitize the index
-	if (is_int( (int)$start_index )) {
-		// We know this is an integer so massage it into what we need.
-		$start_index = (int)$start_index;
-		if ($start_index < 1) {
-			$start_index = 1;
-		}
-	} else {
-		// Set a sane value
-		$start_index = 1;
-	}
+	$start_index = sanitize_int($start_index, 0, 0, PHP_INT_MAX);
 	$start_index = sprintf("%04d", $start_index);
 
+echo '<PRE>YEAR: '.$start_year.' - MONTH: '.$start_month.' - INDEX: '.$start_index.'</PRE>';
 
-	
 }
-
 
 echo '</html>'; # end the page.
 ?>
